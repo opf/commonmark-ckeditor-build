036b6fd7ea498b616ee1af34f38bda165afee5b8
Oliver Günther <mail@oliverguenther.de>
2018-07-04 13:47:57 +0200

    Create work package button macro

---
+++ src/plugins/op-macro-wp-button/utils.js
@@ -0,0 +1,19 @@
+const wpButtonMacroSymbol = Symbol( 'isWpButtonMacroSymbol' );
+import {toWidget, isWidget} from '@ckeditor/ckeditor5-widget/src/utils';
+
+export function toWpButtonMacroWidget( viewElement, writer, label ) {
+	writer.setCustomProperty( wpButtonMacroSymbol, true, viewElement );
+	return toWidget( viewElement, writer, { label: label });
+}
+
+
+export function isWpButtonMacroWidget( viewElement ) {
+	return !!viewElement.getCustomProperty( wpButtonMacroSymbol ) && isWidget( viewElement );
+}
+
+
+export function isWpButtonMacroWidgetselected( selection ) {
+	const viewElement = selection.getSelectedElement();
+
+	return !!( viewElement && isWpButtonMacroWidget( viewElement ) );
+}
