dc93cd39ae9fab8f422f913c6b346b8bedd8b7d5
Oliver Günther <mail@oliverguenther.de>
2020-03-24 11:31:02 +0100

    Bump version to v18

--- src/commonmark/commonmark.js
+++ src/commonmark/commonmark.js
@@ -7,7 +7,7 @@ import CommonMarkDataProcessor from './commonmarkdataprocessor';
 
 // Simple plugin which loads the data processor.
 // eslint-disable-next-line no-unused-vars
-export default function CommonMarkPlugin( editor ) {
-	editor.data.processor = new CommonMarkDataProcessor();
+export default function CommonMarkPlugin(editor) {
+	editor.data.processor = new CommonMarkDataProcessor(editor.editing.view.document);
 }
 
