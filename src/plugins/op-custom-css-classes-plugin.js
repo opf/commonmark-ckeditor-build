01dd6c42919055ac253ce3f144d45169277e03cd
Aleix Suau <info@macrofonoestudio.es>
2020-11-16 13:09:53 +0100

    Custom CSS classes applied to p, h1, h2, h3, h4, h5, h6, li and blockquote

--- src/plugins/op-custom-css-classes-plugin.js
+++ src/plugins/op-custom-css-classes-plugin.js
@@ -3,32 +3,28 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 export default class OpCustomCssClassesPlugin extends Plugin {
 
 	init() {
-		this._addCSSClasses();
+		this._addCustomCSSClasses();
 	}
 
-	_addCSSClasses() {
-		const elementsWithCustomClasses = [
-			'paragraph',
-			'heading1',
-			'heading2',
-			'heading3',
-			'heading4',
-			'heading5',
-			'heading6',
-			'figure',
-			'image',
-			'table',
-			'listItem',
-			'blockQuote',
-		];
+	_addCustomCSSClasses() {
+		const elementsWithCustomClassesNamesMap = {
+			'paragraph': 'p',
+			'heading1': 'h1',
+			'heading2': 'h2',
+			'heading3': 'h3',
+			'heading4': 'h4',
+			'heading5': 'h5',
+			'heading6': 'h6',
+			'listItem': 'list--item',
+			'blockQuote': 'blockquote',
+		};
 
-		elementsWithCustomClasses.forEach(element => {
+		Object.keys(elementsWithCustomClassesNamesMap).forEach(elementKey => {
 			this.editor.conversion.for( 'editingDowncast' ).add( dispatcher => {
-				dispatcher.on( `insert:${element}`, ( evt, data, conversionApi ) => {
+				dispatcher.on( `insert:${elementKey}`, ( evt, data, conversionApi ) => {
 					const viewWriter = conversionApi.writer;
-					console.log(`insert:${element}`, data.item, data.item.getAttribute('listType'), viewWriter, this);
 
-					viewWriter.addClass( `op-${element}`, conversionApi.mapper.toViewElement( data.item ) );
+					viewWriter.addClass( `op-uc-${elementsWithCustomClassesNamesMap[elementKey]}`, conversionApi.mapper.toViewElement( data.item ) );
 				}, { priority: 'low' } );
 			});
 		})
