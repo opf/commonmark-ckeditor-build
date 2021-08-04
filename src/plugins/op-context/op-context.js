846b2124b6683fd1ed2bf52a38ccc437aab37c1c
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-08-04 06:48:34 +0000

    Remove usage of idFromLink from project HalResource (#70)

--- src/plugins/op-context/op-context.js
+++ src/plugins/op-context/op-context.js
@@ -2,14 +2,6 @@ export function getOP(editor) {
 	return _.get(editor.config, '_config.openProject');
 }
 
-export function getOPService(editor, name) {
-	return getPluginContext(editor).services[name];
-}
-
-export function getPluginContext(editor) {
-	return _.get(editor.config, '_config.openProject.pluginContext');
-}
-
 export function getOPResource(editor) {
 	return _.get(editor.config, '_config.openProject.context.resource');
 }
@@ -18,6 +10,18 @@ export function getOPPreviewContext(editor) {
 	return _.get(editor.config, '_config.openProject.context.previewContext');
 }
 
-export function getOPPath(editor, pathName) {
+export function getPluginContext(editor) {
+	return _.get(editor.config, '_config.openProject.pluginContext');
+}
+
+export function getOPService(editor, name) {
+	return getPluginContext(editor).services[name];
+}
+
+export function getOPHelper(editor, name) {
+	return getPluginContext(editor).helpers[name];
+}
+
+export function getOPPath(editor) {
 	return getOPService(editor,'pathHelperService');
 }
