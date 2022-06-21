9fe51d969ea9812f2d2887c8afcff93bfc1a8bb7
Oliver Günther <mail@oliverguenther.de>
2022-06-21 09:51:40 +0200

    Bump ckeditor to 34

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,6 @@ module.exports = {
 	optimization: {
 		minimizer: [
 			new TerserPlugin( {
-				sourceMap: true,
 				terserOptions: {
 					output: {
 						// Preserve CKEditor 5 license comments.
