e3082be4b5242c1a56a609b82790c97a4ec5f2b1
Kabiru Mwenja <makabby@gmail.com>
2023-06-28 13:06:16 +0300

    feat[OP#48583]: Use real emoji store
    Borrowed from [ckeditor4/emoji.json](https://github.com/ckeditor/ckeditor4/blob/master/plugins/emoji/emoji.json)

--- src/mentions/emoji-mentions.js
+++ src/mentions/emoji-mentions.js
@@ -1,16 +1,27 @@
-export function emojiMentions(query) {
-	return new Promise((resolve, reject) => {
-		const database = {
-			'thumbs_up': '👍',
-			'thumbs_down': '👎',
-		}
+import emojis from './emojis.json';
 
-		const matches = Object
-			.keys(database)
-			.filter((name) => name.includes(query))
-			.map((name) => {
-				return { id: `:${name}`, name, text: database[name], type: 'emoji' };
-			})
+export function emojiMentions(query) {
+	function isNameOrKeywords( query, name, keywords ) {
+		if ( name.includes(query) ) {
+			return true;
+		}
+		if ( keywords.length !== 0 ) {
+			for (let i = 0; i < keywords.length; i++ ) {
+				if (keywords[i].includes(query)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	return new Promise((resolve, _reject) => {
+		const emojiStore = emojis;
+		const matches = emojiStore
+			.filter((emoji) => isNameOrKeywords(query, emoji.id, emoji.keywords))
+			.map((emoji) => {
+				return { id: emoji.id, name: emoji.id.replace(/:/g, ""), text: emoji.symbol, type: 'emoji' };
+			});
 
 		resolve(matches);
 	})
