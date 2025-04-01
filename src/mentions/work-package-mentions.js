912964a7651a693feeeffe139b38840e620ccbcc
Oliver Günther <mail@oliverguenther.de>
2025-04-01 15:32:31 +0200

    Update CKEditor, fork mention plugin (#99)

--- src/mentions/work-package-mentions.js
+++ src/mentions/work-package-mentions.js
@@ -1,20 +1,22 @@
-export function workPackageMentions(query) {
-	let editor = this;
-	const url = window.OpenProject.urlRoot + `/work_packages/auto_complete.json`;
-	let base = window.OpenProject.urlRoot + `/work_packages/`;
+export function workPackageMentions(prefix) {
+  return function (query) {
+    let editor = this;
+    const url = window.OpenProject.urlRoot + `/work_packages/auto_complete.json`;
+    let base = window.OpenProject.urlRoot + `/work_packages/`;
 
-	if (editor.config.get('disabledMentions').includes('work_package')) {
-		return [];
-	}
+    if (editor.config.get("disabledMentions").includes("work_package")) {
+      return [];
+    }
 
-	return new Promise((resolve, reject) => {
-		jQuery.getJSON(url, {q: query, scope: 'all'}, collection => {
-			resolve(collection.map(wp => {
-				const id = `#${wp.id}`;
-				const idNumber = wp.id;
+    return new Promise((resolve, reject) => {
+      jQuery.getJSON(url, { q: query, scope: "all" }, collection => {
+        resolve(collection.map(wp => {
+          const id = `${prefix}${wp.id}`;
+          const idNumber = wp.id;
 
-				return {id, idNumber, type: 'work_package', text: id, name: wp.to_s, link: base + wp.id};
-			}));
-		});
-	})
+          return { id, idNumber, type: "work_package", text: id, name: wp.to_s, link: base + wp.id };
+        }));
+      });
+    });
+  };
 }
