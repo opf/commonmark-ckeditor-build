a6cb8c22d33ecd24f87334620ab70805d6829aa8
Oliver Günther <mail@oliverguenther.de>
2026-04-30 09:48:06 +0200

    Show work package ##, ### mentions as quickinfo

--- bin/clean.sh
+++ bin/clean.sh
@@ -5,4 +5,4 @@ BUILD_FOLDER=$(realpath "${OPENPROJECT_CORE}/frontend/src/vendor/ckeditor/")
 
 echo "Clearing current build folder ${BUILD_FOLDER}"
 rm -rf "${BUILD_FOLDER}/ckeditor.*" || true
-rm -rf "${BUILD_FOLDER}/translations/" || true
+rm -rf "${BUILD_FOLDER}/translations/*.js" || true
