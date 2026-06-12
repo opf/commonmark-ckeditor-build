e3a15fb09b7b861f8909162f5b09c8e26c9d55de
Eric Schubert <38206611+Kharonus@users.noreply.github.com>
2026-06-12 13:17:52 +0200

    [#75749] add leading plus icon (#118)
    - https://community.openproject.org/wp/XWI-114
    - add plus icon as leading icon to dropdown
    - remove tooltip

--- src/plugins/op-macro-list-plugin.js
+++ src/plugins/op-macro-list-plugin.js
@@ -1,8 +1,8 @@
 import { Plugin } from '@ckeditor/ckeditor5-core';
-
 import { createDropdown, addToolbarToDropdown } from '@ckeditor/ckeditor5-ui';
 
-import {opMacroPlugins} from "../op-plugins";
+import dropdownIcon from './../icons/plus.svg';
+import { opMacroPlugins } from "../op-plugins";
 
 /**
  * Adding a drop down list of macros to the toolbar.
@@ -13,7 +13,6 @@ export default class OPMacroListPlugin extends Plugin {
 	init() {
 		const editor = this.editor;
 		const disabledPluginNames = (editor.config.get('removePlugins') || []).map(p => p.pluginName);
-		const dropdownTooltip = window.I18n.t('js.editor.macro.dropdown.chose_macro');
 
 		// Skip if we don't have any macros here
 		if (editor.config.get('openProject.disableAllMacros') === true) {
@@ -21,9 +20,9 @@ export default class OPMacroListPlugin extends Plugin {
 		}
 
 		// Register UI component.
-		editor.ui.componentFactory.add( 'macroList', locale => {
+		editor.ui.componentFactory.add('macroList', locale => {
 			const dropdownItems = [];
-			for ( const macroPlugin of opMacroPlugins ) {
+			for (const macroPlugin of opMacroPlugins) {
 				if (disabledPluginNames.indexOf(macroPlugin.pluginName) !== -1) {
 					continue;
 				}
@@ -31,17 +30,17 @@ export default class OPMacroListPlugin extends Plugin {
 				const listItem = editor.ui.componentFactory.create(macroPlugin.buttonName);
 				dropdownItems.push(listItem);
 			}
-			const dropdownView = createDropdown( locale );
+			const dropdownView = createDropdown(locale);
 
 			addToolbarToDropdown(dropdownView, dropdownItems, { isVertical: true, class: 'op-macro-list-button' });
-			dropdownView.buttonView.set( {
+			dropdownView.buttonView.set({
 				isOn: false,
 				withText: true,
-				tooltip: dropdownTooltip,
-				label: window.I18n.t('js.editor.macro.dropdown.macros')
-			} );
+				icon: dropdownIcon,
+				label: window.I18n.t('js.editor.macro.macro_dropdown.caption')
+			});
 
 			return dropdownView;
-		} );
+		});
 	}
 }
